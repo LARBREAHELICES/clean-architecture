e9aa521d630d5eefb3f26fb48874700bd3d14ef5
Sofiene Benamar <Sofiene.benamar@hotmail.com>
2025-04-25 17:33:05 +0100

    test

--- api/main.py
+++ api/main.py
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-# app/main.py
-
-from fastapi import FastAPI, Depends
-from pydantic import BaseModel
-from typing import List
-from sqlmodel import SQLModel
-
-from app.infrastructure.db import SessionDep, engine, get_session, Session
-from app.infrastructure.user_repository import UserRepository
-from app.domain.user_service import UserService
-from app.domain.models import User  # ton modèle SQLModel
-
-# Création des tables au démarrage
-SQLModel.metadata.create_all(engine)
-
-app = FastAPI()
-
-# Pydantic models pour les I/O
-class UserIn(BaseModel):
-    username: str
-    age: int
-
-class UserOut(BaseModel):
-    username: str
-    age: int
-    bonus: int
-
-# Dépendance FastAPI pour obtenir le service
-# def get_user_service():
-#     db = SessionDep()
-#     try:
-#         repo = UserRepository(db)
-        
-#         return UserService(repo)
-#     finally:
-#         db.close()
-
-# on utilise la session pour utiliser le repository et faire les requete vers la db
-# ensuite on utilise le repo dans le service.
-def get_user_service(session: Session = Depends(get_session)):
-    repo = UserRepository(session)
-    
-    return UserService(repo)
-
-
-@app.get("/", tags=["Root"])
-def home():
-    return {"message": "Hello from FastAPI with Docker, PostgreSQL, and Adminer!"}
-
-@app.post("/users", response_model=UserOut)
-def create_user(user: UserIn, service: UserService = Depends(get_user_service)):
-    
-    return service.create_user(user.username, user.age)
-
-@app.get("/users", response_model=List[UserOut])
-def list_users(service: UserService = Depends(get_user_service)):
-    
-    return service.get_all_users()
-=======
 from fastapi import FastAPI
 from app.application.controllers.user_controller import router as user_router
 
@@ -65,4 +5,3 @@ app = FastAPI()
 
 # Inclure les routes utilisateur
 app.include_router(user_router, prefix="/api")
->>>>>>> upstream/main
